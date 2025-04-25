0def1a4523fcf801efae4cbd567eac137da91bfc
Wilson Wuilmer Criollo Jurado <WWCRIOLL@pacifico.fin.ec>
2025-04-25 11:40:06 -0500

    ok

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace API_GIT.Controllers
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+           
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
