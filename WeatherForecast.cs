8117bb03ce6e2996b2e5d5026682653f0cfe3f81
Wilson Wuilmer Criollo Jurado <WWCRIOLL@pacifico.fin.ec>
2025-04-28 15:01:20 -0500

    ok

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,6 +9,8 @@ namespace API_GIT
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string? environment { get; set; } 
     }
 }
 
